b393c3efc22062b1c8c7537849510ac2bd890f1e
raingart <13064767+raingart@users.noreply.github.com>
2024-08-16 15:45:31 +0300

    Update HotkeyInputCapture.js

--- HotkeyInputCapture.js
+++ HotkeyInputCapture.js
@@ -1,43 +1,75 @@
+/**
+ * Recording hotkey input
+ * created by: raingart
+*/
+
+// Function to handle hotkey input management
 function handleHotkeyInput() {
    const inputs = document.body.querySelectorAll('input[type="text"]');
+   let needClearInput = false;
 
-   const handleKeyEvent = (evt) => {
-      const input = evt.target;
+   inputs.forEach((input) => {
+      input.addEventListener('keydown', setShortcut); // Set the shortcut when a key is pressed
+      input.addEventListener('keyup', clearInput); // Clear the input when a key is released
+   });
+
+   /**
+    * Function to get the pressed shortcut based on keyboard events
+    * @param {object} evt - Keyboard event object
+    * @returns {string} - The string representation of the pressed keys as a hotkey
+   */
+   function getPressedShortcut({ ctrlKey, shiftKey, altKey, metaKey, key, code }) {
+      // If the Delete or Backspace key is pressed, return an empty string for clear actions
+      switch (key) {
+         case 'Delete':
+         case 'Backspace':
+            return '';
+      }
+
       const keysMap = [];
 
-      if (evt.key === 'Delete' || evt.key === 'Backspace') {
-         input.value = '';
-         return;
+      // Check and add each modifier key if it's pressed
+      if (ctrlKey) keysMap.push('Ctrl');
+      if (shiftKey) keysMap.push('Shift');
+      if (altKey) keysMap.push('Alt');
+      if (metaKey) keysMap.push('Meta');
+
+      // If a non-modifier key is pressed, add its code to the shortcu
+      if (!['Control', 'Shift', 'Alt', 'Meta'].includes(key)) {
+         needClearInput = false;
+         keysMap.push(code.replace(/^(Key|Digit|Numpad)/, ''));
+      }
+      // If a modifier key is pressed, set needClearInput to true
+      else {
+         needClearInput = true;
+         keysMap.push(''); // add empty to indicate that to continue entering
       }
 
-      if (evt.ctrlKey) keysMap.push('Ctrl');
-      if (evt.shiftKey) keysMap.push('Shift');
-      if (evt.altKey) keysMap.push('Alt');
-      if (evt.metaKey) keysMap.push('Meta');
-
-      if (!['ControlLeft', 'ControlRight', 'ShiftLeft', 'ShiftRight', 'AltLeft', 'AltRight', 'MetaLeft', 'MetaRight'].includes(evt.code)) {
-         keysMap.push(evt.code.replace(/^(Key|Digit)/, ''));
-      }
-
-      const pressedShortcut = keysMap.join('+');
-
-      if (pressedShortcut) {
-         input.value = pressedShortcut;
-         evt.preventDefault();
-      }
+      return keysMap.join('+');
    };
 
-   const handleKeyup = (evt) => {
+   /**
+    * Function to set the input value based on the pressed shortcut
+    * @param {object} evt - Keyboard event object
+   */
+   function setShortcut(evt) {
       const input = evt.target;
-      if (['Control', 'Shift', 'Alt', 'Meta'].includes(input.value.trim())) {
-         input.value = '';
-      }
+
+      // Set the input value to the current pressed shortcut
+      input.value = getPressedShortcut(evt);
+
+      // Prevent the default action of the event
+      evt.preventDefault();
    };
 
-   inputs.forEach(input => {
-      input.addEventListener('keydown', handleKeyEvent);
-      input.addEventListener('keyup', handleKeyup);
-   });
+   /**
+    * Function to clear the input field if needed
+    * @param {object} evt - Keyboard event object
+   */
+   function clearInput({ target }) {
+      if (needClearInput) target.value = '';
+   };
 }
 
+// Add the load event listener to call handleHotkeyInput once when the window loads
 window.addEventListener('load', handleHotkeyInput, { capture: true, once: true });
